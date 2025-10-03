2ed9a9cd3004d2d81e29d0899ca83498930be874
Lee Connelly <lee@leeconnelly.dev>
2025-10-03 23:35:15 +0000

    Cache responses

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\ClearsResponseCache;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Note extends Model
 {
     /** @use HasFactory<\Database\Factories\NoteFactory> */
-    use HasFactory, SoftDeletes;
+    use ClearsResponseCache, HasFactory, SoftDeletes;
 
     public function user(): BelongsTo
     {
